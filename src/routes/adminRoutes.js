2308a3306553ed5f284ea1a3d249fc369d33cb2e
sevgi75 <erbilsevgi97@gmail.com>
2024-03-29 01:21:27 +0300

    adminRoutes added

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,3 +2,27 @@
 /* -------------------------------------------------------
     EXPRESS -STORE APP
 ------------------------------------------------------- */
+
+const router = require("express").Router();
+
+const { Product, Category } = require("../controllers/productsController");
+
+const { isAdmin } = require("../middlewares/auth");
+
+router.route("/products").post(isAdmin, Product.create);
+
+router
+  .route("/products/:id")
+  .put(isAdmin, Product.update)
+  .patch(isAdmin, Product.update)
+  .delete(isAdmin, Product.delete);
+
+router.route("/categories").post(isAdmin, Category.create);
+
+router
+  .route("/categories/:id")
+  .put(isAdmin, Category.update)
+  .patch(isAdmin, Category.update)
+  .delete(isAdmin, Category.delete);
+
+module.exports = router;
